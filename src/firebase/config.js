23b8a1b4d5c9cb98765c01d66f7b6cde0f621715
kabir-webDev <kabir.webdev@gmail.com>
2020-10-01 13:33:27 +0600

    Config file added

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC-NVUj5Sz3eFPxS_RU94DjmC0gFrPNNao",
+  authDomain: "image-gram-360.firebaseapp.com",
+  databaseURL: "https://image-gram-360.firebaseio.com",
+  projectId: "image-gram-360",
+  storageBucket: "image-gram-360.appspot.com",
+  messagingSenderId: "566545611225",
+  appId: "1:566545611225:web:85190f9fe258e29f5b41c8",
+};
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
+
+export { projectStorage, projectFireStore };
